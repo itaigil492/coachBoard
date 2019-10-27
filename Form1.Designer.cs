1a0e62906d411607be819fb40bf03217c6d1261f
NovaCiduki <ciduki@gmail.com>
2019-10-27 18:12:56 +0200

    basic GUI code

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsAppThird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Goldenrod;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(99, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "תפריט";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 85);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "נתונים מסוג א";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Yellow;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button3.Location = new System.Drawing.Point(0, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(191, 85);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "נתונים מסוג ב";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Yellow;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button4.Location = new System.Drawing.Point(0, 182);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(191, 85);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "נתונים מסוג ג";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Yellow;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button5.Location = new System.Drawing.Point(0, 273);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(191, 85);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "נתונים מסוג ד";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Location = new System.Drawing.Point(99, 139);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(191, 0);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button6.Location = new System.Drawing.Point(588, 54);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(191, 85);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "גרף";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 646);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button6;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppThird
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool MenuOpen = false;
+        int NumButton = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MenuOpen == false) 
+            {   
+                panel1.Height += 853;
+                MenuOpen = true;
+            }
+            else if (MenuOpen == true)
+            {
+                panel1.Height -= 853;
+                MenuOpen = false;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (NumButton == 2)
+            {
+                button1.Text = "תפריט";
+                panel1.Height -= 853;
+                MenuOpen = false;
+                NumButton = 0;
+            }
+            else
+            {
+                button1.Text = "נתונים מסוג א";
+                NumButton = 2;
+                panel1.Height -= 853;
+                MenuOpen = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (NumButton == 3)
+            {
+                button1.Text = "תפריט";
+                panel1.Height -= 853;
+                MenuOpen = false;
+                NumButton = 0;
+            }
+            else
+            {
+                button1.Text = "נתונים מסוג ב";
+                NumButton = 3;
+                panel1.Height -= 853;
+                MenuOpen = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (NumButton == 4)
+            {
+                button1.Text = "תפריט";
+                panel1.Height -= 853;
+                MenuOpen = false;
+                NumButton = 0;
+            }
+            else
+            {
+                button1.Text = "נתונים מסוג ג";
+                NumButton = 4;
+                panel1.Height -= 853;
+                MenuOpen = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (NumButton == 5)
+            {
+                button1.Text = "תפריט";
+                panel1.Height -= 853;
+                MenuOpen = false;
+                NumButton = 0;
+            }
+            else
+            {
+                button1.Text = "נתונים מסוג ד";
+                NumButton = 5;
+                panel1.Height -= 853;
+                MenuOpen = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+    }
+}
