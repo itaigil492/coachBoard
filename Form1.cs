58015cd2b3b0bc8aee230698bfeba6682d25ff2c
asaf8888 <asaf2048@gmail.com>
2019-10-30 19:04:57 +0200

    added function to get avg list

--- Form1.cs
+++ Form1.cs
@@ -17,129 +17,151 @@ namespace WindowsFormsAppThird
     public partial class Form1 : Form
     {
         Random RN =new Random();
-     	Dictionary<string, Dictionary<string, Dictionary<string, int>>> TeamInfo = new Dictionary<string, Dictionary<string, Dictionary<string, int>>>();
-		Dictionary<string, Dictionary<string, int>> Game1 = new Dictionary<string, Dictionary<string, int>>();
-		Dictionary<string, Dictionary<string, int>> Game2 = new Dictionary<string, Dictionary<string, int>>();
-		Dictionary<string, Dictionary<string, int>> Game3 = new Dictionary<string, Dictionary<string, int>>();
-		Dictionary<string, int> MisCarGame1 = new Dictionary<string, int>(){
+    	Dictionary<string, int> MisCarGame1 = new Dictionary<string, int>(){
             ("number", 1574),
 		    ("Panel", RN.Next(0,7)),
 		    ("Cargo", RN.Next(0,7))
         };
 		
-		Dictionary<string, int> OrbitGame1 = new Dictionary<string, int>();
-		OrbitGame1.Add("number", 1690);
-		OrbitGame1.Add("Panel", RN.Next(0,7));
-		OrbitGame1.Add("Cargo", RN.Next(0,7));
+		Dictionary<string, int> OrbitGame1 = new Dictionary<string, int>(){
+            ("number", 1690),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		Dictionary<string, int> DbugGame1 = new Dictionary<string, int>(){
+            ("number", 3316),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		Dictionary<string, int> GeneralAngelsGame1 = new Dictionary<string, int>(){
+            ("number", 2230),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
 		
-		Dictionary<string, int> DbugGame1 = new Dictionary<string, int>();
-		DbugGame1.Add("number", 3316);
-		DbugGame1.Add("Panel", RN.Next(0,7));
-		DbugGame1.Add("Cargo", RN.Next(0,7));
+		Dictionary<string, int> GreenBlitzGame1 = new Dictionary<string, int>(){
+            ("number", 4590),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
 		
-		Dictionary<string, int> GeneralAngelsGame1 = new Dictionary<string, int>();
-		GeneralAngelsGame1.Add("number", 2230);
-		GeneralAngelsGame1.Add("Panel", RN.Next(0,7));
-		GeneralAngelsGame1.Add("Cargo", RN.Next(0,7));
-		
-		Dictionary<string, int> GreenBlitzGame1 = new Dictionary<string, int>();
-		GreenBlitzGame1.Add("number", 4590);
-		GreenBlitzGame1.Add("Panel", RN.Next(0,7));
-		GreenBlitzGame1.Add("Cargo", RN.Next(0,7));
-		
-		Dictionary<string, int> TheSpikesGame1 = new Dictionary<string, int>();
-		TheSpikesGame1.Add("number", 2212);
-		TheSpikesGame1.Add("Panel", RN.Next(0,7));
-		TheSpikesGame1.Add("Cargo", RN.Next(0,7));
-		
-		
-				Dictionary<string, int> MisCarGame2 = new Dictionary<string, int>();
-		MisCarGame2.Add("number", 1574);
-		MisCarGame2.Add("Panel", RN.Next(0,7));
-		MisCarGame2.Add("Cargo", RN.Next(0,7));
-		
-		Dictionary<string, int> OrbitGame2 = new Dictionary<string, int>();
-		OrbitGame2.Add("number", 1690);
-		OrbitGame2.Add("Panel", RN.Next(0,7));
-		OrbitGame2.Add("Cargo", RN.Next(0,7));
-		
-		Dictionary<string, int> DbugGame2 = new Dictionary<string, int>();
-		DbugGame2.Add("number", 3316);
-		DbugGame2.Add("Panel", RN.Next(0,7));
-		DbugGame2.Add("Cargo", RN.Next(0,7));
-		
-		Dictionary<string, int> GeneralAngelsGame2 = new Dictionary<string, int>();
-		GeneralAngelsGame2.Add("number", 2230);
-		GeneralAngelsGame2.Add("Panel", RN.Next(0,7));
-		GeneralAngelsGame2.Add("Cargo", RN.Next(0,7));
-		
-		Dictionary<string, int> GreenBlitzGame2 = new Dictionary<string, int>();
-		GreenBlitzGame2.Add("number", 4590);
-		GreenBlitzGame2.Add("Panel", RN.Next(0,7));
-		GreenBlitzGame2.Add("Cargo", RN.Next(0,7));
-		
-		Dictionary<string, int> TheSpikesGame2 = new Dictionary<string, int>();
-		TheSpikesGame2.Add("number", 2212);
-		TheSpikesGame2.Add("Panel", RN.Next(0,7));
-		TheSpikesGame2.Add("Cargo", RN.Next(0,7));
-		
-				Dictionary<string, int> MisCarGame3 = new Dictionary<string, int>();
-		MisCarGame3.Add("number", 1574);
-		MisCarGame3.Add("Panel", RN.Next(0,7));
-		MisCarGame3.Add("Cargo", RN.Next(0,7));
-		
-		Dictionary<string, int> OrbitGame3 = new Dictionary<string, int>();
-		OrbitGame3.Add("number", 1690);
-		OrbitGame3.Add("Panel", RN.Next(0,7));
-		OrbitGame3.Add("Cargo", RN.Next(0,7));
-		
-		Dictionary<string, int> DbugGame3 = new Dictionary<string, int>();
-		DbugGame3.Add("number", 3316);
-		DbugGame3.Add("Panel", RN.Next(0,7));
-		DbugGame3.Add("Cargo", RN.Next(0,7));
-		
-		Dictionary<string, int> GeneralAngelsGame3 = new Dictionary<string, int>();
-		GeneralAngelsGame3.Add("number", 2230);
-		GeneralAngelsGame3.Add("Panel", RN.Next(0,7));
-		GeneralAngelsGame3.Add("Cargo", RN.Next(0,7));
-		
-		Dictionary<string, int> GreenBlitzGame3 = new Dictionary<string, int>();
-		GreenBlitzGame3.Add("number", 4590);
-		GreenBlitzGame3.Add("Panel", RN.Next(0,7));
-		GreenBlitzGame3.Add("Cargo", RN.Next(0,7));
-		
-		Dictionary<string, int> TheSpikesGame3 = new Dictionary<string, int>();
-		TheSpikesGame3.Add("number", 2212);
-		TheSpikesGame3.Add("Panel", RN.Next(0,7));
-		TheSpikesGame3.Add("Cargo", RN.Next(0,7));
+		Dictionary<string, int> TheSpikesGame1 = new Dictionary<string, int>(){
+            ("number", 2212),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
 		
 		
-		Game1.Add("GreenBlitz", GreenBlitzGame1);
-		Game1.Add("MisCar", MisCarGame1);
-		Game1.Add("Orbit", OrbitGame1);
-		Game1.Add("Dbug", DbugGame1);
-		Game1.Add("GeneralAngels", GeneralAngelsGame1);
-		Game1.Add("TheSpikes", TheSpikesGame1);
-		Game2.Add("GreenBlitz", GreenBlitzGame2);
-		Game2.Add("MisCar", MisCarGame2);
-		Game2.Add("Orbit", OrbitGame2);
-		Game2.Add("Dbug", DbugGame2);
-		Game2.Add("GeneralAngels", GeneralAngelsGame2);
-		Game2.Add("TheSpikes", TheSpikesGame2);
-		Game3.Add("GreenBlitz", GreenBlitzGame3);
-		Game3.Add("MisCar", MisCarGame3);
-		Game3.Add("Orbit", OrbitGame3);
-		Game3.Add("Dbug", DbugGame3);
-		Game3.Add("GeneralAngels", GeneralAngelsGame3);
-		Game3.Add("TheSpikes", TheSpikesGame3);
-		TeamInfo.Add("Game1", Game1);
-		TeamInfo.Add("Game2", Game2);
-		TeamInfo.Add("Game3", Game3);
+		Dictionary<string, int> MisCarGame2 = new Dictionary<string, int>(){
+            ("number", 1574),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		
+		Dictionary<string, int> OrbitGame2 = new Dictionary<string, int>(){
+            ("number", 1690),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		
+		Dictionary<string, int> DbugGame2 = new Dictionary<string, int>(){
+            ("number", 3316),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		
+		Dictionary<string, int> GeneralAngelsGame2 = new Dictionary<string, int>(){
+            ("number", 2230),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		
+		Dictionary<string, int> GreenBlitzGame2 = new Dictionary<string, int>(){
+            ("number", 4590),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		
+		Dictionary<string, int> TheSpikesGame2 = new Dictionary<string, int>(){
+            ("number", 2212),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		
+				Dictionary<string, int> MisCarGame3 = new Dictionary<string, int>(){
+            ("number", 1574),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		
+		Dictionary<string, int> OrbitGame3 = new Dictionary<string, int>(){
+            ("number", 1690),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		
+		Dictionary<string, int> DbugGame3 = new Dictionary<string, int>(){
+            ("number", 3316),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		
+		Dictionary<string, int> GeneralAngelsGame3 = new Dictionary<string, int>(){
+            ("number", 2230),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		
+		Dictionary<string, int> GreenBlitzGame3 = new Dictionary<string, int>(){
+            ("number", 4590),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+		
+		Dictionary<string, int> TheSpikesGame3 = new Dictionary<string, int>(){
+            ("number", 2212),
+		    ("Panel", RN.Next(0,7)),
+		    ("Cargo", RN.Next(0,7))
+		};
+
+		
+Dictionary<string, Dictionary<string, int>> Game1 = new Dictionary<string, Dictionary<string, int>>(){
+        ("GreenBlitz", GreenBlitzGame1),
+		("MisCar", MisCarGame1),
+		("Orbit", OrbitGame1),
+		("Dbug", DbugGame1),
+		("GeneralAngels", GeneralAngelsGame1),
+		("TheSpikes", TheSpikesGame1)
+        };
+		Dictionary<string, Dictionary<string, int>> Game2 = new Dictionary<string, Dictionary<string, int>>(){
+        ("GreenBlitz", GreenBlitzGame2),
+		("MisCar", MisCarGame2),
+		("Orbit", OrbitGame2),
+		("Dbug", DbugGame2),
+		("GeneralAngels", GeneralAngelsGame2),
+		("TheSpikes", TheSpikesGame2)
+        };
+        
+		Dictionary<string, Dictionary<string, int>> Game3 = new Dictionary<string, Dictionary<string, int>>(){
+        ("GreenBlitz", GreenBlitzGame3),
+		("MisCar", MisCarGame3),
+		("Orbit", OrbitGame3),
+		("Dbug", DbugGame3),
+		("GeneralAngels", GeneralAngelsGame3),
+		("TheSpikes", TheSpikesGame3) 
+        };
+
+	Dictionary<string, Dictionary<string, Dictionary<string, int>>> TeamInfo = new Dictionary<string, Dictionary<string, Dictionary<string, int>>>(){
+        ("Game1", Game1),
+		("Game2", Game2),
+		("Game3", Game3)
+        };
         public Form1()
         {
             InitializeComponent();
         }
-
+        
+        string jsondata = JsonConvert.SerializeObject(TeamInfo);
         
         bool MenuOpen = false;
         int NumButton = 0;
@@ -240,7 +262,39 @@ namespace WindowsFormsAppThird
         }
 
         public static List<double> Avg(Button button){
-
+            List<double> Output = new List<double>();
+            if (button.Text = "Cargo"){
+            Dictionary<string, Dictionary<string, Dictionary<string, int>>> jsonoutput = JsonConvert.desiarlize<Dictionary<string, Dictionary<string, Dictionary<string, int>>>>(jsondata);
+            double MisCarCargo = (jsonoutput[Game1[MisCarGame1[Cargo]]]+jsonoutput[Game2[MisCarGame2[Cargo]]]+jsonoutput[Game3[MisCarGame3[Cargo]]])/3;
+            double DbugCargo = (jsonoutput[Game1[DbugGame1[Cargo]]]+jsonoutput[Game2[DbugGame2[Cargo]]]+jsonoutput[Game3[DbugGame3[Cargo]]])/3;
+            double OrbitCargo = (jsonoutput[Game1[OrbitGame1[Cargo]]]+jsonoutput[Game2[OrbitGame2[Cargo]]]+jsonoutput[Game3[OrbitGame3[Cargo]]])/3;
+            double GeneralAngelsCargo = (jsonoutput[Game1[GeneralAngelsGame1[Cargo]]]+jsonoutput[Game2[GeneralAngelsGame2[Cargo]]]+jsonoutput[Game3[GeneralAngelsGame3[Cargo]]])/3;
+            double TheSpikesCargo = (jsonoutput[Game1[TheSpikesGame1[Cargo]]]+jsonoutput[Game2[TheSpikesGame2[Cargo]]]+jsonoutput[Game3[TheSpikesGame3[Cargo]]])/3;
+            double GreenBlitzCargo = (jsonoutput[Game1[GreenBlitzGame1[Cargo]]]+jsonoutput[Game2[GreenBlitzGame2[Cargo]]]+jsonoutput[Game3[GreenBlitzGame3[Cargo]]])/3;
+            Output.Add(MisCarCargo);
+            Output.Add(DbugCargo);
+            Output.Add(OrbitCargo);
+            Output.Add(GeneralAngelsCargo);
+            Output.Add(TheSpikesCargo);
+            Output.Add(GreenBlitzCargo);
+            
+            }
+            else if (button.Text = "Panel"){
+            Dictionary<string, Dictionary<string, Dictionary<string, int>>> jsonoutput = JsonConvert.desiarlize<Dictionary<string, Dictionary<string, Dictionary<string, int>>>>(jsondata);
+            double MisCarPanels = (jsonoutput[Game1[MisCarGame1[Panels]]]+jsonoutput[Game2[MisCarGame2[Panels]]]+jsonoutput[Game3[MisCarGame3[Panels]]])/3;
+            double DbugPanels = (jsonoutput[Game1[DbugGame1[Panels]]]+jsonoutput[Game2[DbugGame2[Panels]]]+jsonoutput[Game3[DbugGame3[Panels]]])/3;
+            double OrbitPanels = (jsonoutput[Game1[OrbitGame1[Panels]]]+jsonoutput[Game2[OrbitGame2[Panels]]]+jsonoutput[Game3[OrbitGame3[Panels]]])/3;
+            double GeneralAngelsPanels = (jsonoutput[Game1[GeneralAngelsGame1[Panels]]]+jsonoutput[Game2[GeneralAngelsGame2[Panels]]]+jsonoutput[Game3[GeneralAngelsGame3[Panels]]])/3;
+            double TheSpikesPanels = (jsonoutput[Game1[TheSpikesGame1[Panels]]]+jsonoutput[Game2[TheSpikesGame2[Panels]]]+jsonoutput[Game3[TheSpikesGame3[Panels]]])/3;
+            double GreenBlitzPanels = (jsonoutput[Game1[GreenBlitzGame1[Panels]]]+jsonoutput[Game2[GreenBlitzGame2[Panels]]]+jsonoutput[Game3[GreenBlitzGame3[Panels]]])/3;
+            Output.Add(MisCarPanels);
+            Output.Add(DbugPanels);
+            Output.Add(OrbitPanels);
+            Output.Add(GeneralAngelsPanels);
+            Output.Add(TheSpikesPanels);
+            Output.Add(GreenBlitzPanels);
+            }
+            return(Output);
         }
         public static int RN(int max)  
         {  
